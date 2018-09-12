60d579e7254df6d0b633bc84e17bf783fbef144c
Brandon Patterson <brandnpatterson@gmail.com>
2018-09-11 22:29:14 -0500

    set dev conditional

--- src/store.js
+++ src/store.js
@@ -1,28 +1,21 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import reducers from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
+const devEnv = process.env.NODE_ENV === 'development';
 
-let store;
-
-if (process.env.NODE_ENV === 'development') {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-} else if (process.env.NODE_ENV === 'production') {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware))
-  );
-}
+const store = createStore(
+  reducers,
+  initialState,
+  devEnv
+    ? compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    : compose(applyMiddleware(...middleware))
+);
 
 export default store;
